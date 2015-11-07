80ddc2686e9972f3a7585cfad6d94755fd918a0b
Juli Emmel <justjiffy@gmail.com>
2015-11-07 14:49:50 -0800

    email form working

---
+++ script.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+
+// SCROLL ANIMATION 
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+// end scroll animation
+
